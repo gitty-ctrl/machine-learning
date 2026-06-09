b29858b7f2d6519283b73da1b8f9462923d4641f
gitty-ctrl <123466411+gitty-ctrl@users.noreply.github.com>
2026-06-09 22:44:07 +0100

    added basic finger tracking

--- AI_Mouse/AI_Mouse.py
+++ AI_Mouse/AI_Mouse.py
@@ -1,5 +1,7 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
+import mouse
+import numpy as np
 
 # Initialize hand detector
 detector = HandDetector(detectionCon=0.9, maxHands=1)
@@ -20,6 +22,15 @@ while True:
 
     # Detect hands
     hands, img = detector.findHands(img, flipType=False)
+    if hands:
+        # Get the position of the index finger tip
+        lmlist = hands[0]['lmList']
+        ind_x, ind_y = lmlist[8][0], lmlist[8][1]
+        cv2.circle(img, (ind_x,ind_y),5,(255,0,255),2)
+        # Move the mouse cursor
+        conv_x = int(np.interp(ind_x, [0, cam_w], [0, 1920]))
+        conv_y = int(np.interp(ind_y, [0, cam_h], [0, 1080]))
+        mouse.move(conv_x, conv_y)
 
     # Show Camera Feed
     cv2.imshow("Camera Feed", img)
